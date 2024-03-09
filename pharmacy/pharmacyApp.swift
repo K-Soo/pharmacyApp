793439f3e079991545b8314f051fc9e7e4dfc1f8
K-Soo <kanabun102@naver.com>
2024-03-09 09:59:16 +0900

    Initial Commit

---
+++ pharmacy/pharmacyApp.swift
@@ -0,0 +1,17 @@
+//
+//  pharmacyApp.swift
+//  pharmacy
+//
+//  Created by admin on 2024/03/09.
+//
+
+import SwiftUI
+
+@main
+struct pharmacyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
